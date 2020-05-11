e5e3a45c6a22413534ac77238786cdb3a2c06fc6
Henrique de Castilhos <henrique@castilhos.dev>
2020-05-11 18:55:50 -0300

    chore(theme): add new colors

--- src/common/UI/Layout/Layout.tsx
+++ src/common/UI/Layout/Layout.tsx
@@ -1,39 +1,42 @@
 import React from 'react'
 import { createGlobalStyle, css } from 'styled-components'
 
-import { theme } from '../ThemeProvider'
-
 const GlobalStyle = createGlobalStyle`
-  body,
-  html {
-    background-color: ${theme.colors.light};
-    font-family: ${theme.settings.fontFamilyBase};
-  }
+  ${({ theme }) => css`
+    body,
+    html {
+      background-color: ${theme.colors.light};
+      font-family: ${theme.settings.fontFamilyBase};
+    }
 
-  :root {
-    ${({ theme: { colors, fontSizes } }) => css`
-      /* Colors */
-      --color-primary: ${colors.primary};
-      --color-light: ${colors.light};
-      --color-dark: ${colors.dark};
-      --color-black: ${colors.black};
-      --color-grey: ${colors.grey};
-      --color-white: ${colors.white};
-      --color-error: ${colors.error};
+    :root {
+      ${({ theme: { colors, fontSizes } }) => css`
+        /* Colors */
+        --color-primary: ${colors.primary};
+        --color-light: ${colors.light};
+        --color-dark: ${colors.dark};
+        --color-black: ${colors.black};
+        --color-grey: ${colors.grey};
+        --color-green: ${colors.green};
+        --color-white: ${colors.white};
+        --color-white-light: ${colors['white-light']};
+        --color-white-lighter: ${colors['white-lighter']};
+        --color-error: ${colors.error};
 
-      /* Font sizes */
-      --size-display-1: ${fontSizes['display-1']};
-      --size-display-2: ${fontSizes['display-2']};
-      --size-display-3: ${fontSizes['display-3']};
-      --size-display-4: ${fontSizes['display-4']};
-      --size-headline: ${fontSizes.headline};
-      --size-title: ${fontSizes.title};
-      --size-body-1: ${fontSizes['body-1']};
-      --size-body-2: ${fontSizes['body-2']};
-      --size-caption: ${fontSizes.caption};
-      --size-overline: ${fontSizes.overline};
-    `}
-  }
+        /* Font sizes */
+        --size-display-1: ${fontSizes['display-1']};
+        --size-display-2: ${fontSizes['display-2']};
+        --size-display-3: ${fontSizes['display-3']};
+        --size-display-4: ${fontSizes['display-4']};
+        --size-headline: ${fontSizes.headline};
+        --size-title: ${fontSizes.title};
+        --size-body-1: ${fontSizes['body-1']};
+        --size-body-2: ${fontSizes['body-2']};
+        --size-caption: ${fontSizes.caption};
+        --size-overline: ${fontSizes.overline};
+      `}
+    }
+  `}
 `
 
 const Layout: React.FC = ({ children }) => {
