373a65bd318238bd50e64adf254f6be663f1bd3c
Henrique de Castilhos <henrique@castilhos.dev>
2020-05-11 15:23:43 -0300

    feat(ui): add layout component

--- src/common/UI/index.ts
+++ src/common/UI/index.ts
@@ -1,4 +1,5 @@
 export { Container } from './Grid'
+export { Layout } from './Layout'
 export { theme, ThemeProvider } from './ThemeProvider'
 
 export type { Colors, FontSizes, Theme } from './ThemeProvider'
