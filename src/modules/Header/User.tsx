afbbdffa857fc344478b6926264a17fa7177ff65
Henrique de Castilhos <henrique@castilhos.dev>
2020-05-16 00:51:41 -0300

    feat(header): adds user logged info

--- src/modules/Header/User.tsx
+++ src/modules/Header/User.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 
+import { UserAvatar } from './UserAvatar'
+import { SpotifyContext } from 'common/contexts'
+import { getUserInformation, UserInformation } from 'common/services'
 import { Button, Variant } from 'common/UI'
 import { UserLoginButton } from 'modules/User'
 
 const User: React.FC<{ variant?: Variant }> = ({ variant = 'outline' }) => {
+  const { isAuthenticated, setToken, token } = useContext(SpotifyContext)
+
+  const [userData, setUserData] = useState<UserInformation>()
+
+  const endSession = () => {
+    setToken()
+  }
+
+  useEffect(() => {
+    const getUser = async () => {
+      const user = await getUserInformation(token)
+
+      setUserData(user)
+    }
+
+    if (token) {
+      getUser()
+    }
+  }, [token])
+
+  if (isAuthenticated && userData) {
+    return (
+      <UserAvatar
+        name={userData.userName}
+        image={userData.userImage}
+        logOut={endSession}
+      />
+    )
+  }
+
   return (
     <Button variant={variant} as={UserLoginButton}>
       Log in with Spotify
