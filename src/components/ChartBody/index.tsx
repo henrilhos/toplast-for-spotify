9bd0b06e8cfaf4057d428da1e323d8c087f83252
Henrique de Castilhos <henrique@castilhos.dev>
2021-03-18 22:01:33 -0300

    Refactor chart

--- src/components/ChartBody/index.tsx
+++ src/components/ChartBody/index.tsx
@@ -14,8 +14,6 @@ const useStyles = makeStyles(() =>
     image: {
       width: '170px',
       height: '170px',
-      boxShadow:
-        'rgb(0 0 0 / 20%) 0px 3px 3px -2px, rgb(0 0 0 / 14%) 0px 3px 4px 0px, rgb(0 0 0 / 12%) 0px 1px 8px 0px',
     },
     grid: {
       textAlign: 'center',
--- src/components/ChartFooter/index.tsx
+++ src/components/ChartFooter/index.tsx
@@ -1,33 +1,67 @@
+import { createStyles, makeStyles } from '@material-ui/core'
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 import { Palette } from 'node-vibrant/lib/color'
 
-import * as S from './styles'
 import { getDescriptionByType } from 'services/chart'
 import { GetUserTopArtistsOrTracks } from 'services/spotify'
 
 type Props = { data: GetUserTopArtistsOrTracks; palette?: Palette }
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    siteGrid: {
+      display: 'flex',
+      flexDirection: 'column-reverse',
+    },
+  })
+)
+
 const ChartFooter = ({ data, palette }: Props) => {
+  const classes = useStyles()
   const textColor = palette?.LightMuted?.titleTextColor
   const backgroundColor = palette?.LightMuted?.hex
 
+  const siteTextColor = palette?.DarkMuted?.titleTextColor ?? '#fff'
+  const siteBackgroundColor = palette?.DarkMuted?.hex ?? '#000'
+
   return (
-    <S.Wrapper backgroundColor={backgroundColor} color={textColor}>
-      <S.Container>
-        <S.Image src={data.image} />
+    <Box
+      alignItems="center"
+      color={textColor}
+      display="flex"
+      height="150px"
+      style={{ backgroundColor }}
+      paddingX="12px"
+    >
+      <Grid container spacing={3} style={{ height: '100%' }}>
+        <Grid item xs={8}>
+          {/* Oi */}
+        </Grid>
+        <Grid item xs={4} className={classes.siteGrid}>
+          <Box
+            style={{ backgroundColor: siteBackgroundColor }}
+            padding="1rem"
+            textAlign="center"
+            color={siteTextColor}
+          >
+            <Typography variant="button">spotify.toplast.app</Typography>
+          </Box>
+        </Grid>
+      </Grid>
+      {/* <S.Wrapper backgroundColor={backgroundColor} color={textColor}> */}
+      {/*  <S.Container> */}
+      {/*    <S.Image src={data.image} /> */}
 
-        <S.Content>
-          <S.Description>{getDescriptionByType(data.type)}</S.Description>
-          <S.Title>{data.title}</S.Title>
-          {data.description && <S.Subtitle>{data.description}</S.Subtitle>}
-        </S.Content>
-      </S.Container>
-
-      <S.Container>
-        <S.Content>
-          <S.Title>spotify.toplast.net</S.Title>
-        </S.Content>
-      </S.Container>
-    </S.Wrapper>
+      {/*    <S.Content> */}
+      {/*      <S.Description>{getDescriptionByType(data.type)}</S.Description> */}
+      {/*      <S.Title>{data.title}</S.Title> */}
+      {/*      {data.description && <S.Subtitle>{data.description}</S.Subtitle>} */}
+      {/*    </S.Content> */}
+      {/*  </S.Container> */}
+      {/* </S.Wrapper> */}
+    </Box>
   )
 }
 
