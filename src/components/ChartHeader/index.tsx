d9275612c98069337dbe21468f7de4cdfbb4ed38
Henrique de Castilhos <henrique@castilhos.dev>
2021-03-19 19:04:09 -0300

    Refactor chart header

--- src/components/ChartHeader/index.tsx
+++ src/components/ChartHeader/index.tsx
@@ -9,46 +9,46 @@ import type { GetUserTopArtistsOrTracks } from 'services/spotify'
 type Props = { data: GetUserTopArtistsOrTracks; palette?: Palette }
 type StyleProps = { image: string; bgColor: string; textColor: string }
 
-const useStyles = ({ image, bgColor, textColor }: StyleProps) =>
-  makeStyles(() =>
-    createStyles({
-      container: {
-        backgroundColor: bgColor,
-        color: textColor,
-        height: '325px',
-        position: 'relative',
-      },
-      imageWrapper: {
-        height: '100%',
-        position: 'absolute',
-        right: 0,
-        width: '325px',
-      },
-      image: {
-        backgroundImage: `url(${image})`,
-        backgroundPosition: '50% 25%',
-        backgroundSize: 'cover',
-        height: '100%',
-        position: 'absolute',
-        width: '100%',
-      },
-      overlay: {
-        background: `linear-gradient(90deg, ${bgColor} 5%, transparent 100%)`,
-        height: '100%',
-        left: -5,
-        position: 'absolute',
-        width: '100%',
-      },
-      content: {
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100%',
-        justifyContent: 'center',
-        paddingLeft: '15px',
-        paddingRight: '325px',
-      },
-    })
-  )
+const useStyles = makeStyles(() =>
+  createStyles({
+    container: {
+      backgroundColor: ({ bgColor }: StyleProps) => bgColor,
+      color: ({ textColor }: StyleProps) => textColor,
+      height: '325px',
+      position: 'relative',
+    },
+    imageWrapper: {
+      height: '100%',
+      position: 'absolute',
+      right: 0,
+      width: '325px',
+    },
+    image: {
+      backgroundImage: ({ image }: StyleProps) => `url(${image})`,
+      backgroundPosition: '50% 25%',
+      backgroundSize: 'cover',
+      height: '100%',
+      position: 'absolute',
+      width: '100%',
+    },
+    overlay: {
+      background: ({ bgColor }: StyleProps) =>
+        `linear-gradient(90deg, ${bgColor} 5%, transparent 100%)`,
+      height: '100%',
+      left: -5,
+      position: 'absolute',
+      width: '100%',
+    },
+    content: {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100%',
+      justifyContent: 'center',
+      paddingLeft: '15px',
+      paddingRight: '325px',
+    },
+  })
+)
 
 const ChartHeader = ({ data, palette }: Props) => {
   const textColor = palette?.DarkMuted?.titleTextColor ?? '#fff'
@@ -58,7 +58,7 @@ const ChartHeader = ({ data, palette }: Props) => {
     image: data.image,
     bgColor,
     textColor,
-  })()
+  })
 
   return (
     <Box className={classes.container}>
