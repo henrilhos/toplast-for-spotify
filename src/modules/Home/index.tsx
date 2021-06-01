e000b292c454c9418e8d4beeecca2d922b851d9f
Henrique de Castilhos <henrique@castilhos.dev>
2021-06-01 18:53:49 -0300

    Add time range selector

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -6,8 +6,10 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
 import { useRouter } from "next/router";
-import { useContext, useState } from "react";
+import { MouseEvent, useContext, useState } from "react";
 
 import Login from "components/Login";
 import { ChartContext } from "contexts/Chart";
@@ -28,14 +30,24 @@ function Home() {
   const { handleChart } = useContext(ChartContext);
 
   const classes = useStyles();
-  const [isLoading, setLoading] = useState(false);
   const router = useRouter();
+  const [isLoading, setLoading] = useState(false);
+  const [timeRange, setTimeRange] = useState<
+    "long_term" | "medium_term" | "short_term"
+  >("short_term");
+
+  const handleTimeRange = (
+    event: MouseEvent<HTMLElement>,
+    newAlignment: "long_term" | "medium_term" | "short_term"
+  ) => {
+    setTimeRange(newAlignment);
+  };
 
   const handleChartGeneration = async (type: "artists" | "tracks") => {
     setLoading(true);
 
     if (token) {
-      const data = await getUserTopData({ token, type });
+      const data = await getUserTopData({ token, type, timeRange });
       if (handleChart) {
         handleChart(data);
         return router.push("/image");
@@ -73,6 +85,25 @@ function Home() {
                       <Typography variant="body1" component="p">
                         Choose what you want to see:
                       </Typography>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                      <ToggleButtonGroup
+                        value={timeRange}
+                        exclusive
+                        onChange={handleTimeRange}
+                        aria-label="text formatting"
+                      >
+                        <ToggleButton value="short_term" aria-label="1 month">
+                          1 month
+                        </ToggleButton>
+                        <ToggleButton value="medium_term" aria-label="6 months">
+                          6 months
+                        </ToggleButton>
+                        <ToggleButton value="long_term" aria-label="Overall">
+                          Overall
+                        </ToggleButton>
+                      </ToggleButtonGroup>
                     </Grid>
 
                     <Grid item xs={6}>
